8ef62b5c33c9cb6856d54063d5c377f3c340929f
shobika.palani <shobika.palani@syncfusion.com>
2019-04-04 15:15:56 +0530

    how to customize edit mode behavior of cells in wpf datagrid is committed

---
+++ ViewModel/StudentCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid
+{
+    public class StudentCollection
+    {
+        public StudentCollection()
+        {
+
+            Students = new ObservableCollection<Student>
+            {
+                new Student(1, "Joe Smith ", 3412.45, 42.6),
+                new Student(2, "John Doe", 1223.45, 56.8),
+                new Student(3, "Mary Brown", 12234.6, 36.5),
+                new Student(4, "Joe Smith ", 34535.4, 42.6),
+                new Student(5, "John Doe", 12236.6, 56.8),
+                new Student(6, "Mary Brown", 12455.6, 36.5)
+            };
+        }
+
+        public ObservableCollection<Student> Students { get;  set; }
+    }
+}
